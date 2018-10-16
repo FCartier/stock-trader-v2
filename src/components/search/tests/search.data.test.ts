7e5f79b52663e6d11531eacaf79f3e1810e5f9e9
Florian Cartier <florian.cartier@weareadative.com>
2018-10-16 15:00:25 +0200

    wip: simply test to verify jest is well implemented

--- src/components/search/tests/search.data.test.ts
+++ src/components/search/tests/search.data.test.ts
@@ -1,9 +1,21 @@
 import formatSearchData from '../utils/formatSearchData';
 import unformattedSearchData from "./unformattedSearchData";
 import formattedSearchData from "./formattedSearchData";
+import {api} from '../../../utils/apiUtil';
 
-describe('testing search format utility', () => {
-    test('should calculate max value from formatted data', () => {
-        expect(formatSearchData(unformattedSearchData)).toEqual(formattedSearchData)
-    })
-})
+// describe('testing search format utility', () => {
+//     test('should calculate max value from formatted data', () => {
+//         expect(formatSearchData(unformattedSearchData)).toEqual(formattedSearchData)
+//     })
+// })
+
+test('adds 1 + 2 to equal 3', () => {
+    expect(1 + 2).toBe(3);
+});
+
+test('the data is peanut butter', async () => {
+    expect.assertions(1);
+    await expect(api.getSymbols()).resolves.toBe(unformattedSearchData);
+  });
+  
+
