b6e756b56934bb1358d00a27848445187210bf36
Toby Li <toby.li@weareadaptive.com>
2018-10-25 17:38:49 +0100

    Made footer more responsive

--- src/components/footer/styles.ts
+++ src/components/footer/styles.ts
@@ -1,19 +1,25 @@
-import styled from 'react-emotion'
+import styled from "react-emotion";
 
-export const MarketFoot = styled('ul')`
-    display: flex;
-    font-size: 10px;
-    list-style-type: none;
-    align-items: stretch;
-    justify-content: space-around;
-    background-image: linear-gradient(to bottom right, ${props => props.theme.marketFooterTop}, ${props => props.theme.marketFooterBottom}); 
-    overflow: hidden;
-    width: 100%;
-    padding: 0;
-    margin: 0;
-    height: 5vh;
-    padding-top: 1vh;
-    color: ${props => props.theme.textColor};
-`
+export const MarketFoot = styled("ul")`
+  display: flex;
+  font-size: 10px;
+  list-style-type: none;
+  align-items: center;
+  justify-content: center;
+  background-image: linear-gradient(
+    to bottom right,
+    ${props => props.theme.marketFooterTop},
+    ${props => props.theme.marketFooterBottom}
+  );
+  width: 100%;
+  padding: 0;
+  margin: 0;
+  height: 5vh;
+  color: ${props => props.theme.textColor};
 
-export const MarketItem = styled('li')
+  & li {
+    margin: 0 1rem;
+  }
+`;
+
+export const MarketItem = styled("li");
