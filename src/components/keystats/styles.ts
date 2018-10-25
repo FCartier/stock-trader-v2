dffe8c3d8c0829337059db89d7f5fc5a58f64e67
Toby Li <toby.li@weareadaptive.com>
2018-10-25 16:37:59 +0100

    Responsive key stats

--- src/components/keystats/styles.ts
+++ src/components/keystats/styles.ts
@@ -1,38 +1,49 @@
-import styled from 'react-emotion'
+import styled from "react-emotion";
 
-export const List = styled('ul')`
+export const List = styled("ul")`
   list-style-type: none;
   padding-left: 0;
-  li  {
+
+  li {
     border-bottom: 2px solid grey;
-    p  {
-      display: inline-block;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    flex-wrap: nowrap;
+
+    p {
       width: 49%;
     }
+
     h5 {
-      display: inline-block;
       text-align: right;
       padding: 0px;
       margin: 0px;
       width: 49%;
     }
   }
-`
 
-export const Row = styled('div')`
-  @media (max-width: 1040px) {
+  @media (max-width: 950px) {
+    margin: 0;
+  }
+`;
+
+export const Row = styled("div")`
+  display: flex;
+  justify-content: space-between;
+  flex-wrap: nowrap;
+
+  @media (max-width: 950px) {
     display: block;
-    margin-bottom: 30px;
   }
-  @media (min-width: 1040px) {
-    display: flex;
-    justify-content: space-between;
-  }
-  @media (max-width: 760px) {
-    padding-right: 1rem
-  }
-`
+`;
 
-export const Column = styled('div')`
-  width: 49%
-`
+export const Column = styled("div")`
+  width: 49%;
+
+  @media (max-width: 950px) {
+    width: 100%;
+    margin: 0;
+    padding: 0;
+  }
+`;
