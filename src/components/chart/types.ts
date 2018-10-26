04dee105e6997d29aa325a1ab591f339366979d1
Florian Cartier <florian.cartier@weareadative.com>
2018-10-26 11:55:57 +0200

    fix: remove unsused code

--- src/components/chart/types.ts
+++ src/components/chart/types.ts
@@ -2,15 +2,6 @@ import { fetchStatus } from "../reusables/fetchStatus";
 
 export interface ChartData { input: string, time: string };
 
-export interface ChartEventData {
-  date: string;
-  iexId: string;
-  isEnabled: boolean;
-  name: string;
-  symbol: string;
-  type: string;
-}
-
 export default interface ChartState {
   chart: ChartData[];
   status: fetchStatus;
