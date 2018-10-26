65b4e67c2f2d566fe0f613447c53acf322b13663
Pierre de Leastar <pdeleastar@gmailcom>
2018-10-26 12:14:57 +0200

    market status position moved

--- src/Nav.tsx
+++ src/Nav.tsx
@@ -4,28 +4,30 @@ import Price from "./components/price";
 import Search from "./components/search/Search";
 // @ts-ignore
 import * as logo from "./images/logo.png";
-import { Link } from "react-router-dom"
-
+import { Link } from "react-router-dom";
 
 const Nav = (props: any) => {
-  console.log(props)
+  console.log(props);
   return (
-  <div>
-    <div className="flexbox-container">
-      <div>
-        <Link to="/"><Logo src={logo} alt="Adaptive Logo" /></Link> 
+    <div>
+      <div className="flexbox-container">
+        <div>
+          <Logo src={logo} alt="Adaptive Logo" />
+        </div>
       </div>
-    </div>
-    <div className="flexbox-container searchbar-container">
-      <div className="searchbar">
-       <Search />
-      </div>
-      <div className="price-information">
-        <Price />
-      </div>
-    </div>
-    <hr />
-  </div>
-)};
 
-export default Nav; 
+      <div className="flexbox-container searchbar-container">
+        <div className="searchbar">
+          <Search />
+        </div>
+        <div className="price-information">
+          <Price />
+        </div>
+      </div>
+
+      <hr />
+    </div>
+  );
+};
+
+export default Nav;
