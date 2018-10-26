18f02409ce086e03c352022163f19dff968d177c
Florian Cartier <florian.cartier@weareadative.com>
2018-10-26 11:19:10 +0200

    refacto: clean the code

--- src/components/info/utils/marketStatus.ts
+++ src/components/info/utils/marketStatus.ts
@@ -1,7 +1,6 @@
 export function isStockTrading(openTime: number, closeTime: number) {
   const currentTime = Math.round((new Date()).getTime())
-  console.log("openTime: "+openTime);
-  console.log("closeTIme : "+ closeTime)
+
   const open = formTime(openTime)
   const close = formTime(closeTime)
   const current = formTime(currentTime)
@@ -16,7 +15,6 @@ export function formTime(input: number) {
   const date = new Date(input);
   const hour = date.getHours();
   const minute = date.getMinutes();
-  console.log("formTime Input : "+input)
-  console.log("output : "+ (hour + (minute / 60)))
+
   return (hour + (minute / 60))
 }
