80a54e6ede2f3944e6c583b70373e9cbf42f68ef
Toby Li <toby.li@weareadaptive.com>
2018-10-26 10:20:09 +0100

    footer responsiveness

--- src/components/footer/styles.ts
+++ src/components/footer/styles.ts
@@ -4,15 +4,17 @@ export const MarketFoot = styled("ul")`
   display: flex;
   font-size: 10px;
   list-style-type: none;
-  align-items: center;
+  align-items: stretch;
   justify-content: space-around;
   background-image: linear-gradient(
     to bottom right,
     ${props => props.theme.marketFooterTop},
     ${props => props.theme.marketFooterBottom}
   );
-  width: 100%;
-  padding: 0;
+  overflow: hidden;
+  width: calc(100% - 2rem);
+  padding: 1vh 1rem 0 1rem;
+  
   margin: 0;
   height: 5vh;
   color: ${props => props.theme.textColor};
