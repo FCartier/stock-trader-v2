fb504276fc84030933a2c3daa90ac608b3523347
Florian Cartier <florian.cartier@weareadative.com>
2018-10-26 11:17:53 +0200

    fix: correct the market tests

--- src/components/info/utils/__tests__/marketStatus.tests.ts
+++ src/components/info/utils/__tests__/marketStatus.tests.ts
@@ -1,10 +1,10 @@
 import { isStockTrading, formTime } from "../marketStatus";
 
+// Test is the market is close, should return flase
 test('isStockTrading', () => {
-    expect(isStockTrading(1, 2)).toEqual(true);
+    expect(isStockTrading(1540474200529, 1540497600368)).toBeFalsy();
 });
 
-
 test('formTime', () => {
-    expect(formTime(2)).toBe(3);
+    expect(formTime(1540474200529)).toEqual(15.5);
 });
--- src/components/info/utils/marketStatus.ts
+++ src/components/info/utils/marketStatus.ts
@@ -1,6 +1,7 @@
 export function isStockTrading(openTime: number, closeTime: number) {
   const currentTime = Math.round((new Date()).getTime())
-
+  console.log("openTime: "+openTime);
+  console.log("closeTIme : "+ closeTime)
   const open = formTime(openTime)
   const close = formTime(closeTime)
   const current = formTime(currentTime)
@@ -8,11 +9,14 @@ export function isStockTrading(openTime: number, closeTime: number) {
   if (current > open && current < close) {
     return true;
   }
+  return false;
 }
 
 export function formTime(input: number) {
   const date = new Date(input);
   const hour = date.getHours();
   const minute = date.getMinutes();
+  console.log("formTime Input : "+input)
+  console.log("output : "+ (hour + (minute / 60)))
   return (hour + (minute / 60))
 }
